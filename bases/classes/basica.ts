662dee9e045ecc813149810fca4cdfcd14157801
Javier Ramos Nodal <javiramosnodal@gmail.com>
2023-01-14 08:55:04 +0100

    classes basica.ts

---
+++ bases/classes/basica.ts
@@ -0,0 +1,28 @@
+(() => {
+    class Avenger {
+        // private name: string;
+        // private team: string;
+        // public realName: string;
+        static avgAge: number = 35;
+        static getAvgAge () {
+            return this.name;
+        }
+
+        constructor(
+            private name: string, 
+            private team: string, 
+            public realName?: string
+        ) {}
+
+        bio() {
+            return `${this.name} (${this.team})`
+        }
+
+    }
+
+    const antman = new Avenger("Antman", "Capitán", 'Scott Lang');
+
+    console.log(antman)
+    console.log(antman.bio())
+    console.log(Avenger.getAvgAge())
+})()
--- bases/main.js
+++ bases/main.js
@@ -1,57 +1,25 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class Avenger {
+        static getAvgAge() {
+            return this.name;
         }
-    return t;
-};
-(() => {
-    const avengers = {
-        nick: 'Samuel L. Jackson',
-        ironman: 'Robert Downey Jr.',
-        vision: 'Paul Bettany',
-        activo: true,
-        poder: 1500
-    };
-    const { poder, vision } = avengers;
-    console.log(poder.toFixed(2), vision.toUpperCase());
-    const printAvenger = (_a) => {
-        var { ironman } = _a, resto = __rest(_a, ["ironman"]);
-        console.log(ironman, resto);
-    };
-    printAvenger(avengers);
-    const avengersArr = ['Cap. América', true, 150.1];
-    // const ironman = avengersArr[1];
-    const [, ironman,] = avengersArr;
-    console.log(ironman);
-})();
-(() => {
-    const ironman = {
-        name: 'Ironman',
-        weapon: 'Armorsuit'
-    };
-    const captainAmerica = {
-        name: 'Capitán América',
-        weapon: 'Escudo'
-    };
-    const thor = {
-        name: 'Thor',
-        weapon: 'Mjolnir'
-    };
-    const avengers = [ironman, captainAmerica, thor];
-    for (const avenger of avengers) {
-        console.log(avenger);
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
-})();
-(() => {
-    const nombre = 'Fernando';
-    const getName = () => {
-        console.log('viejo getName');
-    };
+    // private name: string;
+    // private team: string;
+    // public realName: string;
+    Avenger.avgAge = 35;
+    const antman = new Avenger("Antman", "Capitán", 'Scott Lang');
+    console.log(antman);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
